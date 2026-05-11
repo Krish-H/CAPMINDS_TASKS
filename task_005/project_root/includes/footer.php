34cf39f8256d1a60b09630e8977959f5a983881b
Hari Krishnan <144368103+Krish-H@users.noreply.github.com>
2026-05-11 09:35:50 +0530

    Update footer.php

--- task_005/project_root/includes/footer.php
+++ task_005/project_root/includes/footer.php
@@ -1,5 +1,16 @@
-</div> <!-- End container -->
+<footer class=" text-black mt-5">
+
+    <div class=" text-center py-3 small">
+
+        © 2026 Hospital Management System | Developed using PHP, MySQL & Bootstrap
+
+    </div>
+
+</footer>
+
 <!-- Bootstrap JS -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
