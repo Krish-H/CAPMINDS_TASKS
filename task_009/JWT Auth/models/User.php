d193d38b88cb70bc439f0f8e43609ad4c6fafc6e
Hari Krishnan <144368103+Krish-H@users.noreply.github.com>
2026-05-25 09:12:32 +0530

    Update User.php

--- task_009/JWT Auth/models/User.php
+++ task_009/JWT Auth/models/User.php
@@ -36,7 +36,7 @@ class User
 
     public function findByEmail($email)
     {
-        $query = "SELECT * FROM " . $this->table . " WHERE email = :email LIMIT 1";
+        $query = "SELECT id,name,email,password FROM " . $this->table . " WHERE email = :email LIMIT 1";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':email', $email);
         $stmt->execute();
